bb70387ce25399712bfe97b559fbe062434c5a77
HaDucDev <haductrung392000@gmail.com>
2023-02-01 17:55:07 +0700

    modelMapper-config

---
+++ beshop/src/main/java/com/haduc/beshop/config/UtilsConfig.java
@@ -0,0 +1,20 @@
+package com.haduc.beshop.config;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UtilsConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STANDARD);
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+        return modelMapper;
+    }
+}
