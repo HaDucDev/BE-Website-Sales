455e2c4bd1a7afdffd27e5032831837a19ff3e09
HaDucDev <haductrung392000@gmail.com>
2023-03-03 23:02:45 +0700

    update-start-momo

--- beshop/src/main/java/com/haduc/beshop/service/impl/OrderServiceImpl.java
+++ beshop/src/main/java/com/haduc/beshop/service/impl/OrderServiceImpl.java
@@ -104,43 +104,45 @@ public class OrderServiceImpl implements IOrderService {
 
         if (createOrderResquest.getMethodPayment().equals("tien_mat")) {
             order.setNote(ConstantValue.STATUS_ORDER_NO_TT);
+            order.setStatusOrder(ConstantValue.STATUS_ORDER_NOT_APPROVED);
+            order.setCreatedDate(new Date(System.currentTimeMillis()));
+
+            System.out.println("den day 1");
+            User user = this.iUserRepository.findByUserIdAndIsDeleteFalse(createOrderResquest.getUserId())
+                    .orElseThrow(() -> new NotXException("Không tìm thấy người dùng", HttpStatus.NOT_FOUND));
+            order.setUser(user);
+
+            //list san pahm dat mua
+
+            List<Cart> cartList = this.iCartRepository.findById_UserIdAndIsDeleteFalse(createOrderResquest.getUserId());
+            System.out.println("mang ban dau co   "+ cartList.size());
+            List<Cart> cartBuyList = cartList.stream().filter((item)-> createOrderResquest.getBuyProducts().contains(item.getId().getProductId())).collect(Collectors.toList());
+            // tong tien
+            Integer totalOrder = cartBuyList.stream()
+                    .reduce(0, (initTotal, item)-> initTotal + (totalSellingPrice(item.getProduct(), item.getQuantity())), Integer::sum);
+
+            order.setTotalAmount(Long.valueOf(totalOrder));// chuyên sang do dat la Long
+            Order order1= this.iOrderRepository.save(order);
+
+            if(!cartBuyList.isEmpty()){
+                cartBuyList.forEach((item)->{
+                    OrderDetail orderDetail = new OrderDetail();
+                    orderDetail.setId(new OrderDetailIDKey(order1.getOrdersId(),item.getId().getProductId()));
+                    orderDetail.setQuantity(item.getQuantity());
+                    orderDetail.setAmount(Long.valueOf(totalSellingPrice(item.getProduct(),item.getQuantity())));
+                    this.iOrderDetailRepository.save(orderDetail);
+                    this.iCartRepository.deleteProductFromCart(item.getId());
+                    if(cartBuyList.isEmpty()){
+                        return;
+                    }
+                });
+            }
+            return new MessageResponse("Bạn đã tạo đơn hàng thành công với id đơn hàng là:" + order1.getOrdersId());
         }
         if (createOrderResquest.getMethodPayment().equals("tn_momo")) {
             order.setNote(ConstantValue.STATUS_ORDER_YES_TT_MOMO_GG);
+            
         }
-        order.setStatusOrder(ConstantValue.STATUS_ORDER_NOT_APPROVED);
-        order.setCreatedDate(new Date(System.currentTimeMillis()));
-
-        System.out.println("den day 1");
-        User user = this.iUserRepository.findByUserIdAndIsDeleteFalse(createOrderResquest.getUserId())
-                .orElseThrow(() -> new NotXException("Không tìm thấy người dùng", HttpStatus.NOT_FOUND));
-        order.setUser(user);
-
-        //list san pahm dat mua
-
-        List<Cart> cartList = this.iCartRepository.findById_UserIdAndIsDeleteFalse(createOrderResquest.getUserId());
-        System.out.println("mang ban dau co   "+ cartList.size());
-        List<Cart> cartBuyList = cartList.stream().filter((item)-> createOrderResquest.getBuyProducts().contains(item.getId().getProductId())).collect(Collectors.toList());
-        // tong tien
-        Integer totalOrder = cartBuyList.stream()
-                .reduce(0, (initTotal, item)-> initTotal + (totalSellingPrice(item.getProduct(), item.getQuantity())), Integer::sum);
-
-        order.setTotalAmount(Long.valueOf(totalOrder));// chuyên sang do dat la Long
-        Order order1= this.iOrderRepository.save(order);
-
-        if(!cartBuyList.isEmpty()){
-            cartBuyList.forEach((item)->{
-                OrderDetail orderDetail = new OrderDetail();
-                orderDetail.setId(new OrderDetailIDKey(order1.getOrdersId(),item.getId().getProductId()));
-                orderDetail.setQuantity(item.getQuantity());
-                orderDetail.setAmount(Long.valueOf(totalSellingPrice(item.getProduct(),item.getQuantity())));
-                this.iOrderDetailRepository.save(orderDetail);
-                this.iCartRepository.deleteProductFromCart(item.getId());
-                if(cartBuyList.isEmpty()){
-                    return;
-                }
-            });
-        }
-        return new MessageResponse("Bạn đã tạo đơn hàng thành công với id đơn hàng là:" + order1.getOrdersId());
+        return null;
     }
 }
