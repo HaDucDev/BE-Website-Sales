13b305c419df90adbf7937586fbaa637acb29f02
HaDucDev <haductrung392000@gmail.com>
2023-03-04 12:24:56 +0700

    done-payment-onine-momo

--- beshop/src/main/java/com/haduc/beshop/service/impl/OrderServiceImpl.java
+++ beshop/src/main/java/com/haduc/beshop/service/impl/OrderServiceImpl.java
@@ -137,7 +137,7 @@ public class OrderServiceImpl implements IOrderService {
                 orderDetail.setQuantity(item.getQuantity());
                 orderDetail.setAmount(Long.valueOf(totalSellingPrice(item.getProduct(), item.getQuantity())));
                 this.iOrderDetailRepository.save(orderDetail);
-                //this.iCartRepository.deleteProductFromCart(item.getId());
+                this.iCartRepository.deleteProductFromCart(item.getId());
                 if (cartBuyList.isEmpty()) {
                     return;
                 }
@@ -173,32 +173,28 @@ public class OrderServiceImpl implements IOrderService {
     @Override
     public void handleOrderAfterPaymentMoMo(MomoIPNRequest request) {
         String idOrderMoMoSend = request.getOrderId();
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        String username = authentication.getName();
-//        System.out.println("lay ra   " + username);
         String[] idOrderMoMoSendAray = idOrderMoMoSend.split("hdshop");
-        System.out.println("id don hang gui den"+idOrderMoMoSendAray.length);
-        String getOrderId = idOrderMoMoSendAray[idOrderMoMoSendAray.length-1];// lay so cuoi la id
-        System.out.println("id don hang can sua"+getOrderId);
-        Order order = this.iOrderRepository.findById(Integer.parseInt(getOrderId)).orElseThrow(()-> new NotXException("Không tìm thấy đơn hàng này", HttpStatus.NOT_FOUND));
+        System.out.println("id don hang gui den" + idOrderMoMoSendAray.length);
+        String getOrderId = idOrderMoMoSendAray[idOrderMoMoSendAray.length - 1];// lay so cuoi la id
+        System.out.println("id don hang can sua" + getOrderId);
+        Order order = this.iOrderRepository.findById(Integer.parseInt(getOrderId)).orElseThrow(() -> new NotXException("Không tìm thấy đơn hàng này", HttpStatus.NOT_FOUND));
 
-        System.out.println("don hang lay dc"+order.getNote());
+        System.out.println("don hang lay dc" + order.getNote());
         if (request.getErrorCode() == 0) {
             // Đơn hàng đã thanh toán thành công
-            // Xử lý tương ứng với trạng thái này ở đây
             System.out.println("Bạn đã thanh toán thành công với đơn hàng có id" + request.getOrderId());
-            if(order.getNote().equals(ConstantValue.STATUS_ORDER_YES_TT_MOMO_GG)){
+            if (order.getNote().equals(ConstantValue.STATUS_ORDER_YES_TT_MOMO_GG)) {
                 order.setNote(ConstantValue.STATUS_ORDER_YES_TT_MOMO_SUCCES);
             }
             //
         } else {
             // Đơn hàng không thanh toán thành công
             String errorMessage = request.getMessage();
-            if(order.getNote().equals(ConstantValue.STATUS_ORDER_YES_TT_MOMO_GG)){
+            if (order.getNote().equals(ConstantValue.STATUS_ORDER_YES_TT_MOMO_GG)) {
                 order.setNote(ConstantValue.STATUS_ORDER_FAIL_TT_MOMO_GG);
             }
-            System.out.println("error-status" + request.getErrorCode());
-            System.out.println("Bạn đã gặp lỗi" + errorMessage);
+            System.out.println();
+            System.out.println("Bạn đã gặp lỗi error-status" + request.getErrorCode() + " - nội dung: " + errorMessage);
         }
     }
 }
